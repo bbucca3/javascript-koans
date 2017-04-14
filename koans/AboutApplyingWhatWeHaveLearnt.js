25c3eb51bf344e7d8ec74ca287457b0dfddac5a9
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-04-13 20:07:11 -0400

    started AboutApplyingWhatWeHaveLearned.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,24 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var hasMushrooms = function (topping) { return topping === "mushrooms"; }
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      function ediblePizza (pizza) { 
+        if (_(pizza.ingredients).any(hasMushrooms) === false && pizza.containsNuts === false) {
+          return true;
+        }
+      }
+      
+      productsICanEat = products.filter(ediblePizza);
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,7 +61,7 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
